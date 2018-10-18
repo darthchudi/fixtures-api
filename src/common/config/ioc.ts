2e17664efaadaf32dd8a6af44c76925431c50472
chudi <chukwudioranu@ymail.com>
2018-10-18 13:49:34 +0100

    feat: create ioc container

---
+++ src/common/config/ioc.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+import { Container } from 'inversify';
+
+import CONSTANTS from '../constants/identifiers';
+
+import UserRepository from '../../data/respositories/user';
+import FixtureRepository from '../../data/respositories/fixture';
+import TeamRepository from '../../data/respositories/team';
+
+import '../../server/controllers';
+
+const container = new Container();
+container.bind<UserRepository>(CONSTANTS.USER_REPOSITORY).to(UserRepository);
+container
+  .bind<FixtureRepository>(CONSTANTS.FIXTURE_REPOSITORY)
+  .to(FixtureRepository);
+container.bind<TeamRepository>(CONSTANTS.TEAM_REPOSITORY).to(TeamRepository);
+
+export default container;
