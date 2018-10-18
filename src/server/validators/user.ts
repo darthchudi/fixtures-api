0fc86ca8d7cd6a0d2425b72bf0c9d49dc2e73d94
chudi <chukwudioranu@ymail.com>
2018-10-18 17:14:26 +0100

    feat: create user validator class

---
+++ src/server/validators/user.ts
@@ -0,0 +1,50 @@
+import { IUser } from '../../data/types/user';
+import BaseValidator from './base';
+
+class UserValidator extends BaseValidator<IUser> {
+  signUp(body: any) {
+    const schema = this.joi.object({
+      email: this.joi
+        .string()
+        .email()
+        .required(),
+      first_name: this.joi.string().required(),
+      last_name: this.joi.string().required(),
+      role: this.joi
+        .string()
+        .trim()
+        .valid(['admin', 'user']),
+      username: this.joi
+        .string()
+        .min(4)
+        .required(),
+      password: this.joi
+        .string()
+        .required()
+        .min(5),
+      phone_number: this.joi
+        .string()
+        .required()
+        .length(11),
+    });
+
+    return this.validate(body, schema, null);
+  }
+
+  login(body: any) {
+    const schema = this.joi.object({
+      username: this.joi
+        .string()
+        .min(4)
+        .required(),
+      password: this.joi
+        .string()
+        .required()
+        .trim(),
+    });
+
+    return this.validate(body, schema, null);
+  }
+}
+
+export default new UserValidator();
