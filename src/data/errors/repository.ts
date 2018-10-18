7ae5fb09246e39e37eb6d4057b8174d0fec22c68
chudi <chukwudioranu@ymail.com>
2018-10-18 13:50:35 +0100

    feat: add error code to repository errors

--- src/data/errors/repository.ts
+++ src/data/errors/repository.ts
@@ -1,19 +1,25 @@
 namespace RepositoryErrors {
   export class GenericDBError extends Error {
+    code: string;
     constructor(message: string) {
       super(message);
+      this.code = 'NonRetryableError';
     }
   }
 
   export class ModelNotFoundError extends Error {
+    code: string;
     constructor(message: string) {
       super(message);
+      this.code = 'NonRetryableError';
     }
   }
 
   export class DuplicateModelError extends Error {
+    code: string;
     constructor(message: string) {
       super(message);
+      this.code = 'NonRetryableError';
     }
   }
 }
