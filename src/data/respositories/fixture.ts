d3d8d2b81b7248863fa3a8fa1c996fe161a45b9f
chudi <chukwudioranu@ymail.com>
2018-10-18 13:48:52 +0100

    feat: make repositories injectable

--- src/data/respositories/fixture.ts
+++ src/data/respositories/fixture.ts
@@ -1,8 +1,10 @@
 import BaseRepository from './base';
+import { injectable } from 'inversify';
 
 import { IFixtureModel } from '../models/fixture';
 import FixtureSchema from '../schemas/fixture';
 
+@injectable()
 export default class FixtureRepository extends BaseRepository<IFixtureModel> {
   constructor() {
     super('Fixtures', FixtureSchema, 'fixture');
--- src/data/respositories/user.ts
+++ src/data/respositories/user.ts
@@ -1,8 +1,10 @@
 import BaseRepository from './base';
+import { injectable } from 'inversify';
 
 import { IUserModel } from '../models/user';
 import UserSchema from '../schemas/user';
 
+@injectable()
 export default class UserRepository extends BaseRepository<IUserModel> {
   constructor() {
     super('Users', UserSchema, 'user');
