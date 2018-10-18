b4c4783eb1ebe3636e7fe3bcdd8c91ea37d3293c
chudi <chukwudioranu@ymail.com>
2018-10-18 23:52:54 +0100

    feat: extend and customize base repo method in user repo

--- src/data/respositories/user.ts
+++ src/data/respositories/user.ts
@@ -4,9 +4,30 @@ import { injectable } from 'inversify';
 import { IUserModel } from '../models/user';
 import UserSchema from '../schemas/user';
 
+import RepositoryErrors from '../errors/repository';
+
 @injectable()
 export default class UserRepository extends BaseRepository<IUserModel> {
   constructor() {
     super('Users', UserSchema, 'user');
+  }
+
+  create(atttributes: any): Promise<IUserModel> {
+    return new Promise<IUserModel>((resolve, reject) => {
+      super.create(atttributes).then(
+        result => {
+          resolve(result);
+        },
+        err => {
+          if (err.code !== 11000) return reject(err);
+
+          return reject(
+            new RepositoryErrors.DuplicateModelError(
+              'Username or email already exists'
+            )
+          );
+        }
+      );
+    });
   }
 }
