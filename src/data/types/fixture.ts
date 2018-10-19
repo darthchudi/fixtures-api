329ffb607d0854312fee8bea0ed4b215f7e124aa
chudi <chukwudioranu@ymail.com>
2018-10-19 11:22:13 +0100

    chore: make status field enumerable

--- src/data/types/fixture.ts
+++ src/data/types/fixture.ts
@@ -2,6 +2,12 @@ import { Timestamps } from './';
 import { IScore } from './score';
 import { ITeam } from './team';
 
+enum IMatchStatus {
+  pending = 'pending',
+  completed = 'completed',
+  ongoing = 'ongoing',
+}
+
 export interface IFixture extends Timestamps {
   away_team: string | ITeam;
   competition: string;
@@ -11,6 +17,7 @@ export interface IFixture extends Timestamps {
   match_day: number;
   stadium: string;
   stage: string;
+  status: IMatchStatus;
   score: IScore;
   time: string;
 }
