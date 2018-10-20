6681d1599ac3cd29a8b5c7b9e08b89c36ae8412e
chudi <chukwudioranu@ymail.com>
2018-10-20 08:41:46 +0100

    fix: update fixture validator

--- src/server/validators/fixture.ts
+++ src/server/validators/fixture.ts
@@ -23,8 +23,16 @@ class FixtureValidator extends BaseValidator<IFixture> {
 
   update(body: any) {
     const schema = this.joi.object({
-      date: this.joi.date().required(),
-      time: this.joi.string().required(),
+      date: this.joi.date(),
+      time: this.joi.string(),
+      match_day: this.joi.number(),
+      stadium: this.joi.string(),
+      status: this.joi.string().valid(['pending', 'ongoing', 'completed']),
+      score: this.joi.object({
+        winner: this.joi.string(),
+        home_team: this.joi.number().required(),
+        away_team: this.joi.number().required(),
+      }),
     });
 
     return this.validate(body, schema, null);
